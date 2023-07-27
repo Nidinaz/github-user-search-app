ce3559726a856594333775e3aa9592755d0c467e
Dorina Kamp <dorina.kamp@gmail.com>
2023-07-27 15:03:50 +0200

    clearing and adding files for start project

---
+++ components/layout.js
@@ -0,0 +1,7 @@
+import "./globals.css";
+import styles from '../styles/layout.module.css';
+
+
+export default function Layout({ children }) {
+  return <div className={styles.container}>{children}</div>;
+}
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className={styles.main}> Read here </h1>
+    </div>
+  );
+}
